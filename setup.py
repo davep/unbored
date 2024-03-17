76548e7129c7b68043f19673f4f8c18e7423486d
Dave Pearson <davep@davep.org>
2024-03-17 18:07:31 +0000

    :card_index: Fix the way we specify the Textual version

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 ##############################################################################
 # Python imports.
-from pathlib    import Path
-from setuptools import setup, find_packages
+from pathlib import Path
+
+from setuptools import find_packages, setup
 
 ##############################################################################
 # Import the library itself to pull details out of it.
 import unbored
+
 
 ##############################################################################
 # Work out the location of the README file.
@@ -17,7 +19,8 @@ def readme():
     :returns: The path to the README file.
     :rtype: ~pathlib.Path
     """
-    return Path( __file__).parent.resolve() / "README.md"
+    return Path(__file__).parent.resolve() / "README.md"
+
 
 ##############################################################################
 # Load the long description for the package.
@@ -27,36 +30,34 @@ def long_desc():
     :returns: The long description.
     :rtype: str
     """
-    with readme().open( "r", encoding="utf-8" ) as rtfm:
+    with readme().open("r", encoding="utf-8") as rtfm:
         return rtfm.read()
+
 
 ##############################################################################
 # Perform the setup.
 setup(
-
-    name                          = "unbored",
-    version                       = unbored.__version__,
-    description                   = str( unbored.__doc__ ),
-    long_description              = long_desc(),
-    long_description_content_type = "text/markdown",
-    url                           = "https://github.com/davep/unbored",
-    author                        = unbored.__author__,
-    author_email                  = unbored.__email__,
-    maintainer                    = unbored.__maintainer__,
-    maintainer_email              = unbored.__email__,
-    packages                      = find_packages(),
-    package_data                  = { "unbored": [ "py.typed", "unbored.css" ] },
-    include_package_data          = True,
-    install_requires              = [ "textual=>0.52.1", "bored-api", "xdg" ],
-    python_requires               = ">=3.10",
-    keywords                      = "todo fun inspiration api-client terminal textual",
-    entry_points                  = {
-        "console_scripts": "unbored=unbored.app:run"
-    },
-    license                       = (
+    name="unbored",
+    version=unbored.__version__,
+    description=str(unbored.__doc__),
+    long_description=long_desc(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/davep/unbored",
+    author=unbored.__author__,
+    author_email=unbored.__email__,
+    maintainer=unbored.__maintainer__,
+    maintainer_email=unbored.__email__,
+    packages=find_packages(),
+    package_data={"unbored": ["py.typed", "unbored.css"]},
+    include_package_data=True,
+    install_requires=["textual>=0.52.1", "bored-api", "xdg"],
+    python_requires=">=3.10",
+    keywords="todo fun inspiration api-client terminal textual",
+    entry_points={"console_scripts": "unbored=unbored.app:run"},
+    license=(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
     ),
-    classifiers                   = [
+    classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Environment :: Console",
         "Development Status :: 3 - Alpha",
@@ -67,9 +68,8 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Topic :: Utilities",
         "Topic :: Terminals",
-        "Typing :: Typed"
-    ]
-
+        "Typing :: Typed",
+    ],
 )
 
 ### setup.py ends here
